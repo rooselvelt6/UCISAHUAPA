aa4ec048e3707e09ac8319f159bedc246cf4c09d
rooselvelt <rooselvelt6@gmail.com>
2018-08-16 20:50:33 -0400

    nuevas apps para UCI

---
+++ extra-addons/apache/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Apache",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Sistema de gravedad Apache II, es un índice que mide la gravedad de un paciente y estima la mortalidad del mismo
+        cada 24H es una UCI basado en parámetros fisiológicos del paciente
+    """,
+
+    'author': "Rooselvelt Angulo",
+    'website': "",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
